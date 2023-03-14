298163c6b368e4a9ba0fbdfd9268783dac6b521e
Mateusz <xmaten13@gmail.com>
2023-03-14 11:03:05 +0100

    feat: replace own implementation with existing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,5 @@
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
-
-import { Board } from "@/components/Board/Board"
-import { Game } from "@/controllers/Game"
-import { useMemo } from "react"
+import { Game } from "@/components/Game/Game"
 
 export default function Home() {
-  const game = useMemo(() => new Game(), [])
-
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <DndProvider backend={HTML5Backend}>
-        <Board game={game} />
-      </DndProvider>
-    </div>
-  )
+  return <Game />
 }
